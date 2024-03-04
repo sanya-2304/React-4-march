dc2c203a13aae3b445545cba200210cdc37a659a
sanya-2304 <sanyaadoda@gmail.com>
2024-03-04 18:43:31 +0530

    vite react added

---
+++ basic-react-01/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <h1>
+      React Day 01  
+    </h1>
+  );
+}
+
+export default App;
---
+++ vite-react-01/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+  <h1>
+    React day 01 with vite
+  </h1>    
+  )
+}
+
+export default App
